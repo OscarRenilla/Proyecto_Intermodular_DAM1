d84688da4daa520aafff235f97bf9fbc145da09f
Óscar Reni <oscar06renilla@gmail.com>
2026-04-20 14:03:47 +0200

    Modificación del css y de las clases cryptPassword, RelojService y UsuarioService

--- Backend/src/main/java/dam/code/service/RelojService.java
+++ Backend/src/main/java/dam/code/service/RelojService.java
@@ -1,4 +1,53 @@
 package dam.code.service;
 
+import dam.code.dao.RelojDAO;
+import dam.code.dao.impl.RelojDAOImpl;
+
+import dam.code.exception.RelojException;
+import dam.code.models.Reloj;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.time.LocalDate;
+
 public class RelojService {
+
+    private final RelojDAO relojDAO = new RelojDAOImpl();
+
+    public ObservableList<Reloj> obtenerRelojs() {
+        return FXCollections.observableArrayList(relojDAO.listar());
+    }
+
+    public void addVisualizacion(int idUsuario, Reloj reloj) throws RelojException {
+        relojDAO.comprar(idUsuario, reloj.getId());
+
+    }
+
+    public void agregarReloj(Reloj reloj) {
+        validar_reloj(reloj);
+
+        relojDAO.registrar(reloj);
+    }
+
+    private void validar_reloj(Reloj reloj) throws RelojException {
+        if (reloj.getNombre().length() < 2) {
+            throw new RelojException("El nombre del reloj es muy corto");
+        }
+        if (reloj.getModelo().length() < 2) {
+            throw new RelojException("El modelo del reloj es muy corto");
+        }
+        if (reloj.getDescripcion().length() < 2) {
+            throw new RelojException("La descripción del reloj es muy corta");
+        }
+        if (reloj.getStock() < 0){
+            throw new RelojException("El stock no puede ser negativo");
+        }
+        if (reloj.getPrecio() < 0){
+            throw new RelojException("El precio no puede ser negativo");
+        }
+    }
+
+    public ObservableList<Reloj> obtenerRelojsPorUsuario(int idUsuario) throws RelojException {
+        return FXCollections.observableArrayList(relojDAO.obtenerRelojsPorUsuario(idUsuario));
+    }
 }
--- Backend/src/main/java/dam/code/service/UsuarioService.java
+++ Backend/src/main/java/dam/code/service/UsuarioService.java
@@ -1,4 +1,46 @@
 package dam.code.service;
 
+import dam.code.dao.UsuarioDAO;
+import dam.code.dao.impl.UsuarioDAOImpl;
+import dam.code.exception.UsuarioException;
+import dam.code.models.Usuario;
+
 public class UsuarioService {
+    private final UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
+
+    public Usuario login(String dni, String password) {
+        return usuarioDAO.login(dni, password);
+    }
+
+    public void registrar(Usuario usuario, String password) throws UsuarioException {
+        validarPassword(password);
+        validarEmail(usuario.getEmail());
+        usuarioDAO.registrar(usuario, password);
+    }
+
+    private void validarEmail(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        if (!email.matches(regex)) {
+            throw new UsuarioException("El formato del email no es válido");
+        }
+    }
+
+    private void validarPassword(String password) throws UsuarioException {
+        if (password.length() < 6) {
+            throw new UsuarioException("La contraseña tiene que ser mínimo de 6 caracteres");
+        }
+        if (!password.matches(".*[a-z].*")) {
+            throw new UsuarioException("La contraseña debe contener al menos una minúscula");
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            throw new UsuarioException("La contraseña debe contener al menos una mayúscula");
+        }
+        if (!password.matches(".*[@$!%*?&].*")) {
+            throw new UsuarioException("La contraseña debe conter al menos uno de estos símbolos[@$!%*?&]");
+        }
+        if  (!password.matches(".*\\d.*")) {
+            throw new UsuarioException("La contraseña debe contener al menos un número");
+        }
+    }
+
 }
