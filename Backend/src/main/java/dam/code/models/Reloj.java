971ba617d59f32880000975e0005ae4cc880fded
SebaZ-jpg <sebastianalonsoaceroguiop@gmail.com>
2026-04-20 14:39:34 +0200

    models listo

--- Backend/src/main/java/dam/code/models/Reloj.java
+++ Backend/src/main/java/dam/code/models/Reloj.java
@@ -1,14 +1,7 @@
 package dam.code.models;
-/*
-*
-nombre
-modelo
-descripción
-stock
-precio
-* */
 
 public class Reloj {
+    private int id;
     private String nombre;
     private String modelo;
     private String descripcion;
@@ -16,5 +9,78 @@ public class Reloj {
     private double precio;
     private int compras;
 
+    public Reloj(String nombre, String modelo, String descripcion, int stock, double precio, int compras) {
+        this.nombre = nombre;
+        this.modelo = modelo;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.precio = precio;
+        this.compras = compras = 0;
+    }
 
+    public Reloj(int id, String nombre, String modelo, String descripcion, int stock, double precio, int compras){
+        this.id = id;
+        this.nombre = nombre;
+        this.modelo = modelo;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.precio = precio;
+        this.compras = compras = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCompras() {
+        return compras;
+    }
+
+    public void setCompras(int compras) {
+        this.compras = compras;
+    }
 }
