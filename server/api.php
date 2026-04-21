cf70d06532ba28e14eb8533532c1dc05bc78d31f
Óscar Reni <oscar06renilla@gmail.com>
2026-04-21 09:07:07 +0200

    Creación de dos archivos del server .oho

---
+++ server/api.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'db_config.php';
+
+header('Content-Type: application/json');
+
+// Permitir lanzar la api en todos los navegadores
+header('Access-Control-Allow-Origin: *');
+
+$pdo = conectarBBDD();
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+if($metodo === 'GET'){
+
+    $stmt = $pdo -> query("SELECT * FROM tabla WHERE ...")
+
+    //EJEMPLO 
+    $usuarios = $stmt->fetchAll();
+
+    echo json_encode($usuarios);
+
+}elseif ($metodo === 'POST'){
+
+    $datos = json_decode(file_get_contents('php://input'), true);
+
+    $stmt = $pdo -> prepare("INSERT INTO tabla (campos) VALUES (valores)");
+
+    $stmt -> execute([
+
+        ':valor' => $datos['campo'],
+        ':valor' => $datos['campo'],
+    ]);
+
+    echo json_encode([
+        'ok' => true,
+        'message' => 'mensaje de confirmacion'
+    ]);
+
+}
---
+++ server/db_config.php
@@ -0,0 +1,36 @@
+<?php
+
+define('DB_HOST', 'localhost'),
+define('DB_NAME', 'nombre_bbdd'),
+define('DB_USER', 'root'),
+define('DB_PASS', 'password_usuario_postgre'),
+
+function conectarBBDD(): PDO {
+
+    // describe el tipo de base de datos que es y su host
+    $dsn = "pgsql:host=" . DB_HOST . ";port=5432; dbname = " . DB_NAME;
+
+    // comportamiento de la conexión de la bbdd
+    $opciones = [
+
+        // Lanza excepciones en caso de error
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+
+        // Resultados de las consultas 
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+
+    ]
+
+    try{    
+
+        $conn = new PDO($dsn, DB_USER, DB_PASS, $opciones);
+        return $conn
+
+    }catch(PDOException $e){
+        http_response_code(500);
+
+        echo json_encode(['error' => 'Error con la conexión a la base de datos' . $e -> getMessage()]);
+
+        exit;
+    }
+}
